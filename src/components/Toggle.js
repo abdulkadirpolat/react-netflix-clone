8e2bdd4ed88aa0bc5c05edf033af9f42b3b7dbbc
abdulkadirpolat <tr.abdulkadir.polat@gmail.com>
2021-10-30 18:34:08 +0300

    Initial commit

---
+++ src/components/Toggle.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { AiOutlinePlus, AiOutlineClose } from "react-icons/ai";
+
+const Toggle = ({ items, key }) => {
+  const [actived, setActived] = useState(false);
+  return (
+    <li key={key} className="bg-nx-gray-700 mb-2">
+      <button
+        onClickCapture={() => setActived(!actived)}
+        className="border-b border-black w-full lgm:p-7 p-5 smm3:py-3 smm3:px-5 text-left relative"
+      >
+        {items.title}
+        {!actived ? (
+          <AiOutlinePlus className="absolute right-3 bottom-1/4 lgm:bottom-1/3 text-4xl smm3:text-2xl " />
+        ) : (
+          <AiOutlineClose className="absolute right-3 bottom-1/4 lgm:bottom-1/3 text-4xl smm3:text-2xl " />
+        )}
+      </button>
+      <div className={`mt-3 p-7 max-h-3xl ${actived ? "" : "hidden"}`}>
+        {items.text}
+      </div>
+    </li>
+  );
+};
+
+export default Toggle;
